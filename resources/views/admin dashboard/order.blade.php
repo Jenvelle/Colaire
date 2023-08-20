6f9834f6bad2f7246f566425210f22765f9d8024
Shamel <jennzaep@gmail.com>
2023-08-20 14:11:36 +0800

    Changes on some files, added shopping cart css.

---
+++ resources/views/admin dashboard/order.blade.php
@@ -0,0 +1,158 @@
+<div class="card">
+    <div class="card-header">
+        <h5>Orders</h5>
+    </div>
+        <div class="card-body">
+        <div class="table-responsive table-desi">
+          <table class="display dataTable no-footer" id="basic-1" role="grid" aria-describedby="basic-1_info">
+            <thead>
+              <tr role="row">
+                <th class="sorting_asc" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
+                  aria-sort="ascending" aria-label="Order Id: activate to sort column descending"
+                  style="width: 77px;">Order Id</th>
+                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
+                  aria-label="Product: activate to sort column ascending" style="width: 136px;">Product</th>
+                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
+                  aria-label="Payment Status: activate to sort column ascending" style="width: 183px;">Payment Status
+                </th>
+                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
+                  aria-label="Payment Method: activate to sort column ascending" style="width: 152px;">Payment Method
+                </th>
+                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
+                  aria-label="Order Status: activate to sort column ascending" style="width: 117px;">Order Status</th>
+                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
+                  aria-label="Date: activate to sort column ascending" style="width: 91px;">Date</th>
+                <th class="sorting" tabindex="0" aria-controls="basic-1" rowspan="1" colspan="1"
+                  aria-label="Total: activate to sort column ascending" style="width: 72px;">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr role="row" class="odd">
+                <td class="sorting_1">#51240</td>
+                <td>
+                  <div class="d-flex align-items-center">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded">
+                  </div>
+                </td>
+                <td><span class="badge badge-secondary">Cash On Delivery</span></td>
+                <td>Paypal</td>
+                <td><span class="badge badge-success">Delivered</span></td>
+                <td>Dec 10, 2023</td>
+                <td>$54671</td>
+              </tr>
+              <tr role="row" class="even">
+                <td class="sorting_1">#51241</td>
+                <td>
+                  <div class="d-flex align-items-center">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded">
+                  </div>
+                </td>
+                <td><span class="badge badge-success">Paid</span></td>
+                <td>Master Card</td>
+                <td><span class="badge badge-primary">Shipped</span></td>
+                <td>Feb 15, 2023</td>
+                <td>$2136</td>
+              </tr>
+              <tr role="row" class="odd">
+                <td class="sorting_1">#51242</td>
+                <td><img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded"></td>
+                <td><span class="badge badge-warning">Awaiting Authentication</span>
+                </td>
+                <td>Debit Card</td>
+                <td><span class="badge badge-warning">Processing</span></td>
+                <td>Mar 27, 2023</td>
+                <td>$8791</td>
+              </tr>
+              <tr role="row" class="even">
+                <td class="sorting_1">#51243</td>
+                <td>
+                  <div class="d-flex align-items-center">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded">
+                  </div>
+                </td>
+                <td><span class="badge badge-danger">Payment Failed</span></td>
+                <td>Master Card</td>
+                <td><span class="badge badge-danger">Cancelled</span></td>
+                <td>Sep 1, 2023</td>
+                <td>$139</td>
+              </tr>
+              <tr role="row" class="odd">
+                <td class="sorting_1">#51244</td>
+                <td>
+                  <div class="d-flex align-items-center">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded">
+                  </div>
+                </td>
+                <td><span class="badge badge-success">Paid</span></td>
+                <td>Paypal</td>
+                <td><span class="badge badge-primary">Shipped</span></td>
+                <td>May 18, 2023</td>
+                <td>$4678</td>
+              </tr>
+              <tr role="row" class="even">
+                <td class="sorting_1">#51245</td>
+                <td>
+                  <div class="d-flex align-items-center">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded">
+                  </div>
+                </td>
+                <td><span class="badge badge-success">Paid</span></td>
+                <td>Visa</td>
+                <td><span class="badge badge-success">Delivered</span></td>
+                <td>Jan 14, 2023</td>
+                <td>$6791</td>
+              </tr>
+              <tr role="row" class="odd">
+                <td class="sorting_1">#51246</td>
+                <td><img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded"></td>
+                <td><span class="badge badge-warning">Awaiting Authentication</span>
+                </td>
+                <td>Credit Card</td>
+                <td><span class="badge badge-warning">Processing</span></td>
+                <td>Apr 22, 2023</td>
+                <td>$976</td>
+              </tr>
+              <tr role="row" class="even">
+                <td class="sorting_1">#51247</td>
+                <td>
+                  <div class="d-flex align-items-center">
+                    <img src="#" alt="" class="img-fluid img-30 me-2 blur-up lazyloaded">
+                    <img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded">
+                  </div>
+                </td>
+                <td><span class="badge badge-danger">Payment Failed</span></td>
+                <td>Master Card</td>
+                <td><span class="badge badge-danger">Cancelled</span></td>
+                <td>Mar 26, 2023</td>
+                <td>$3212</td>
+              </tr>
+              <tr role="row" class="odd">
+                <td class="sorting_1">#51248</td>
+                <td><img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded"></td>
+                <td><span class="badge badge-secondary">Cash On Delivery</span></td>
+                <td>Paypal</td>
+                <td><span class="badge badge-primary">Shipped</span></td>
+                <td>Feb 29, 2023</td>
+                <td>$6719</td>
+              </tr>
+              <tr role="row" class="even">
+                <td class="sorting_1">#51249</td>
+                <td><img src="#" alt="" class="img-fluid img-30 blur-up lazyloaded"></td>
+                <td><span class="badge badge-success">Paid</span></td>
+                <td>Master Card</td>
+                <td><span class="badge badge-secondary">Processing</span></td>
+                <td>Sep 2, 2023</td>
+                <td>$9765</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        </div>
+</div>
---
+++ resources/views/admin dashboard/transactions.blade.php
@@ -0,0 +1,157 @@
+<div class="card">
+    <div class="card-header">
+        <h5>Transaction</h5>
+    </div>
+        <div class="card-body">
+            <div class="table-responsive table-desi">
+                <table class="table trans-table all-package">
+                <thead>
+              <tr>
+                <th>Order Id</th>
+                <th>Transaction Id</th>
+                <th>Date</th>
+                <th>Payment Method</th>
+                <th>Delivery Status</th>
+                <th>Amount</th>
+              </tr>
+                </thead>
+
+                <tbody>
+              <tr>
+                <td>142</td>
+                <td>#212145214510</td>
+                <td>Jul 20, 2023</td>
+                <td>Paypal</td>
+                <td>Pending</td>
+                <td>$175/-</td>
+              </tr>
+
+              <tr>
+                <td>217</td>
+                <td>#784561421721</td>
+                <td>Jul 25, 2023</td>
+                <td>Paypal</td>
+                <td>Process</td>
+                <td>$845/-</td>
+              </tr>
+
+              <tr>
+                <td>546</td>
+                <td>#476547821142</td>
+                <td>Jul 29, 2023</td>
+                <td>Cash On Deliery</td>
+                <td>Delivered</td>
+                <td>$314/-</td>
+              </tr>
+
+              <tr>
+                <td>671</td>
+                <td>#745384127541</td>
+                <td>Jul 30, 2023</td>
+                <td>Paypal</td>
+                <td>Pending</td>
+                <td>$217/-</td>
+              </tr>
+
+              <tr>
+                <td>565</td>
+                <td>#96725125102</td>
+                <td>Aug 01, 2023</td>
+                <td>Cash On Delivery</td>
+                <td>Process</td>
+                <td>$542/-</td>
+              </tr>
+
+              <tr>
+                <td>754</td>
+                <td>#547121023651</td>
+                <td>Aug 10, 2023</td>
+                <td>Cash On Delivery</td>
+                <td>Pending</td>
+                <td>$2141/-</td>
+              </tr>
+
+              <tr>
+                <td>164</td>
+                <td>#876412242215</td>
+                <td>Aug 18, 2023</td>
+                <td>Paypal</td>
+                <td>Delivered</td>
+                <td>$4872/-</td>
+              </tr>
+
+              <tr>
+                <td>841</td>
+                <td>#31534221621</td>
+                <td>Aug 29, 2023</td>
+                <td>Paypla</td>
+                <td>Process</td>
+                <td>$7841/-</td>
+              </tr>
+
+              <tr>
+                <td>354</td>
+                <td>#78412457421</td>
+                <td>Sep 09, 2023</td>
+                <td>Paypal</td>
+                <td>Pending</td>
+                <td>$2784/-</td>
+              </tr>
+
+              <tr>
+                <td>784</td>
+                <td>#241524757448</td>
+                <td>Sep 17, 2023</td>
+                <td>Cash On Delivery</td>
+                <td>Delivered</td>
+                <td>$461/-</td>
+              </tr>
+
+              <tr>
+                <td>142</td>
+                <td>#212145214510</td>
+                <td>Sep 20, 2023</td>
+                <td>Cash On Delivery</td>
+                <td>Pending</td>
+                <td>$175/-</td>
+              </tr>
+
+              <tr>
+                <td>217</td>
+                <td>#784561421721</td>
+                <td>Dec 10, 2023</td>
+                <td>Cash On Delivery</td>
+                <td>Process</td>
+                <td>$845/-</td>
+              </tr>
+
+              <tr>
+                <td>546</td>
+                <td>#476547821142</td>
+                <td>Feb 15, 2023</td>
+                <td>Cash On Delivery</td>
+                <td>Delivered</td>
+                <td>$314/-</td>
+              </tr>
+
+              <tr>
+                <td>671</td>
+                <td>#745384127541</td>
+                <td>Mar 27, 2023</td>
+                <td>Paypal</td>
+                <td>Pending</td>
+                <td>$217/-</td>
+              </tr>
+
+              <tr>
+                <td>565</td>
+                <td>#96725125102</td>
+                <td>Sep 1, 2023</td>
+                <td>Cash On Delivery</td>
+                <td>Process</td>
+                <td>$542/-</td>
+              </tr>
+                </tbody>
+            </div>
+      </div>
+</div>
